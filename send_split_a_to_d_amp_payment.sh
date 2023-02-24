5c8ec8b29638f53738aee40e6f58586b2adafacb
Erik Ek <erik.ek@gmail.com>
2023-02-24 18:46:29 +0800

    Add files via upload

---
+++ send_split_a_to_d_amp_payment.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo "Send amp payment from alice to dave..."
+
+cd dave
+echo "In dave directory"
+lnclid="lncli --rpcserver=localhost:10004 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon addinvoice --amt=50 --amp"
+addinvoice_command=$($lnclid)
+
+echo $addinvoice_command | jq -r '.payment_request' > ../dave_pay_req.txt
+#read tes <($addinvoice_command | jq -r '.payment_request')
+#t=$kk | jq -r '.payment_request'
+#echo $tes
+
+cd ../alice
+echo "in alice directory:"
+
+dave_pay_req=$(< ../dave_pay_req.txt)
+echo $dave_pay_req
+lnclia="lncli --rpcserver=localhost:10001 --macaroonpath=data/chain/bitcoin/simnet/admin.macaroon"
+eval "$lnclia sendpayment --dest=03a6e8efb135441017d342461ff3ab98720185df905ed02f555e99775949c5fd1d --amp --pay_req=$dave_pay_req"
+
+
